87d63fb73ce17831b788ae2b4660e0410a0a549e
Vesa Kilpiäinen <vesakilp@google.com>
2016-03-23 08:04:13 +0200

    TSL secured server

--- server.js
+++ server.js
@@ -1,24 +1,26 @@
-'use strict';
+var tls = require('tls');
+var fs = require('fs');
 
-const express = require('express');
+//TODO how to get these securely in container
+var options = {
+  key: fs.readFileSync('../cert/server-key.pem'),
+  cert: fs.readFileSync('../cert/server-cert.pem')
+};
 
-// Constants
-const PORT = 8080;
-
-// App
-const app = express();
-const telegramBotToken = "214343648:AAEKHOC2AnxDPg7OW_vEBtb2mY4HGxNKw6k";
-//TODO url should contain telegram bot token
-//https://www.example.com/<token>
-//http://192.168.99.100:49160/214343648:AAEKHOC2AnxDPg7OW_vEBtb2mY4HGxNKw6k/kakkasuu
-//DOES BOT TAKE URL OR IMAGE AS RETURN VALUE, CALL SOME BOT FUNCTION? https://core.telegram.org/bots/api#inlinequeryresultgif
-//https://api.telegram.org/bot214343648:AAEKHOC2AnxDPg7OW_vEBtb2mY4HGxNKw6k/
-//1. take parameter from url
-//2. use parameter to get url OR image from giffy random
-//3. send url OR image back to bot
-app.get('/', function (req, res) {
-  res.send('Hello world\n');
+var server = tls.createServer(options, function (socket) {
+  console.log('server connected', socket.authorized ? 'authorized' : 'unauthorized');
+  socket.on('error', function(error) {
+    console.log('error occured', error);
+  });
+  socket.write('welcome!\n');
+  socket.on('data', function (data) {
+    console.log('data', data.toString());
+    //socket.end();
+  });
+  socket.pipe(socket);
+  socket.end();
 });
 
-app.listen(PORT);
-console.log('Running on http://localhost:' + PORT);
+server.listen(8443, () => {
+  console.log('server bound on 8443');
+});
